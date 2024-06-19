426c3c51a2f32d6e30ea9e774d97039680f8cef3
Imran Ahmed <37019732+ahmedimran35@users.noreply.github.com>
2024-06-19 16:10:58 +0600

    Update web.py

--- web.py
+++ web.py
@@ -4,6 +4,15 @@ import music
 import yt_dlp
 import uuid
 from streamlit.components.v1 import html, components
+# Hide Streamlit elements with custom CSS immediately after page config
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 # Function to delete temporary audio files
